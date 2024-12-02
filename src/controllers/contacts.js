cae3da1b39e4c964d9460fedb32e8536aa185f67
Yulia Boishtian <boishtyan93@gmail.com>
2024-12-03 00:02:06 +0200

    hw5

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,6 +55,7 @@ export const createContactController = async (req, res) => {
     status: 201,
     message: 'Successfully created a contact!',
     data: contact,
+    // data: contact,
   });
 };
 
