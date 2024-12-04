d4e02aac18ecb0d3ecdf0a94ec0b0f859b61a77b
Yulia Boishtian <boishtyan93@gmail.com>
2024-12-04 20:38:33 +0200

    fix patch res

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -83,7 +83,7 @@ export const patchContactController = async (req, res) => {
   res.status(200).json({
     status: 200,
     message: `Successfully updated the contact!`,
-    data: result,
+    data: result.contact,
   });
 };
 
